bf0ae456253b57c2584158486304234e816d72c8
Giovani Schiar <giovanischiar@gmail.com>
2022-11-17 13:45:40 -0800

    Initial Commit

---
+++ RuleOfThree/RuleOfThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuleOfThreeApp.swift
+//  RuleOfThree
+//
+//  Created by Giovani Schiar on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RuleOfThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
